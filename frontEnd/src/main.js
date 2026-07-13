709531f5acf3f6a693ca5e92cce8991a53ad2428
Parham Saravani <parhamsaravani83@gmail.com>
2026-07-13 23:32:40 +0330

    feat: implemented  productPage price slider

--- frontEnd/src/main.js
+++ frontEnd/src/main.js
@@ -2,6 +2,7 @@ import "./style.css";
 import "./utils/theme/theme.js";
 import "./utils/menu/menu.js";
 import './utils/auth/authPage.js'
+import './utils/product/price.js'
 
 import AOS from "aos";
 import "aos/dist/aos.css";
